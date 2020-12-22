a287b546a477a0189b2dfc7e05253f9e619a8b2b
kos <kosmos2uh@gmail.com>
2020-12-22 21:00:32 +0200

    profile

--- include/menu.php
+++ include/menu.php
@@ -4,4 +4,5 @@
     <a class="nav-link" href="/detail.php">Detail</a>
     <a class="nav-link" href="/info.php">Info</a>
     <a class="nav-link" href="/contacts.php">Contacts</a>
+    <a class="nav-link" href="/profile.php">Profile</a>
 </div>
