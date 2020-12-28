0476e874f1080bdd43a3257a84bb313992582087
kos <kosmos2uh@gmail.com>
2020-12-28 20:26:32 +0200

    contact form

--- contacts.php
+++ contacts.php
@@ -1,38 +1,5 @@
 <?php
 include "include/header.php";
-
-$mail_sended = false;
-$form_submitted = false;
-$error_message = '';
-
-if(!empty($_POST)) {
-
-    $form_submitted = true;
-
-    $name = $_POST['contact_name'] ?? '';
-    $email = $_POST['contact_email'] ?? '';
-    $phone = $_POST['contact_phone'] ?? '';
-    $message = $_POST['contact_message'] ?? '';
-
-    $mail_message = 'Дата: ' . date("j.m.Y H:i:s") . PHP_EOL;
-    $mail_message .= 'Имя: ' . $name . PHP_EOL;
-    $mail_message .= 'Email: ' . $email . PHP_EOL;
-    $mail_message .= 'Телефон: ' . $phone . PHP_EOL;
-    $mail_message .= 'Текст сообщения: ' . $message . PHP_EOL . PHP_EOL;
-
-//    $result = mail('admin@mysite.com', 'Новый запрос с фоормы', $mail_message);
-
-    try {
-        $file = $_SERVER['DOCUMENT_ROOT'] . '/log/mail.log';
-        $handler = fopen($file, 'a');
-        fwrite($handler, $mail_message);
-        fclose($handler);
-        $mail_sended = true;
-    } catch (Exception $e) {
-        $error_message = $e->getMessage();
-    }
-
-}
 ?>
     <h1>Контакты</h1>
     <div class="row mb-3">
@@ -45,7 +12,7 @@ if(!empty($_POST)) {
                 <li>Email: email@email.com</li>
             </ul>
             <h4>Связаться с нами</h4>
-            <form method="post" action="">
+            <form method="post" action="/send.php">
                 <div class="mb-3">
                     <label for="contact_name" class="form-label">Ваше имя</label>
                     <input type="text" class="form-control" id="contact_name" name="contact_name" required>
@@ -66,17 +33,13 @@ if(!empty($_POST)) {
                     <button type="submit" class="btn btn-primary">Отправить сообещние</button>
                 </div>
             </form>
-            <?php if($form_submitted) { ?>
-                <?php if($mail_sended) { ?>
-                <div class="alert alert-success" role="alert">
-                    Ваше сообщение успешно отправлено
+            <?php if(isset($_SESSION['mail_form'])) { ?>
+                <div class="alert alert-<?php echo $_SESSION['mail_form']['status'] == 1 ? 'success' : 'danger'; ?>" role="alert">
+                    <?php echo $_SESSION['mail_form']['message']; ?>
                 </div>
-                <?php } else { ?>
-                <div class="alert alert-danger" role="alert">
-                    <?php echo $error_message; ?>
-                </div>
-                <?php } ?>
-            <?php } ?>
+            <?php
+            unset($_SESSION['mail_form']);
+            } ?>
         </div>
         <div class="col-4">
             <?php include "include/right_popular_news.php" ?>
---
+++ send.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if(!empty($_POST)) {
+
+    $_SESSION['mail_form'] = [
+        'status' => 0,
+        'message' => '',
+    ];
+
+    $name = $_POST['contact_name'] ?? '';
+    $email = $_POST['contact_email'] ?? '';
+    $phone = $_POST['contact_phone'] ?? '';
+    $message = $_POST['contact_message'] ?? '';
+
+    $mail_message = 'Дата: ' . date("j.m.Y H:i:s") . PHP_EOL;
+    $mail_message .= 'Имя: ' . $name . PHP_EOL;
+    $mail_message .= 'Email: ' . $email . PHP_EOL;
+    $mail_message .= 'Телефон: ' . $phone . PHP_EOL;
+    $mail_message .= 'Текст сообщения: ' . $message . PHP_EOL . PHP_EOL;
+
+//    $result = mail('admin@mysite.com', 'Новый запрос с фоормы', $mail_message);
+
+    try {
+        $file = $_SERVER['DOCUMENT_ROOT'] . '/log/mail.log';
+        $handler = fopen($file, 'a');
+        fwrite($handler, $mail_message);
+        fclose($handler);
+        $_SESSION['mail_form']['status'] = 1;
+        $_SESSION['mail_form']['message'] = 'Ваше сообщение успешно отправлено';
+    } catch (Exception $e) {
+        $_SESSION['mail_form']['message'] = $e->getMessage();
+    }
+
+}
+
+header("Location: /contacts.php");
